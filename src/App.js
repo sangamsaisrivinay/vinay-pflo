1f15b0b852746e91c06e7512a03330954f0a1751
sangamsaisrivinay <saisrivinayreddysangam@gmail.com>
2023-06-17 07:40:09 +0530

    removed lazy load components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Banner, Schools, Footer } from './pages';
+import {About, Skills, Projects} from './pages';
 
 const LazyAbout= lazy(()=>import ('./pages/About'))
 const LazySkills= lazy(()=>import ('./pages/Skills'))
@@ -9,15 +10,18 @@ const App = () => {
   return (
     <div className="App">
       <Banner/>
-      <Suspense fallback={<div>Loading...</div>}>
+      {/* <Suspense fallback={<div>Loading...</div>}>
         <LazyAbout/>
       </Suspense>
-      {/* <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
         <LazySkills/>
-      </Suspense> */}
+      </Suspense>
       <Suspense fallback={<div>Loading...</div>}>
         <LazyProjects/>
-      </Suspense>
+      </Suspense> */}
+      <About/>
+      <Skills/>
+      <Projects/>
       <Schools/>
       <Footer/>
     </div>
